031c49a29d0d00ae93d9161a6c899b439f110c58
TDF360 <thibautwismer@gmail.com>
2026-07-23 20:09:25 +0200

    Timer UX

--- Assets/Scripts/Timer/Timer.cs
+++ Assets/Scripts/Timer/Timer.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public float origineTime;
     [HideInInspector] public float time;
+    public GameObject dizaine;
+    public GameObject unite;
 
     public Reset reset;
 
@@ -16,18 +19,25 @@ public class Timer : MonoBehaviour
         StartCoroutine(CountDown());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public IEnumerator CountDown()
     {
         while (time > 0.0f)
         {
-            Debug.Log(time);
-            yield return new WaitForSeconds(1.0f);
+            if (time > 9.0f)
+            {
+                int dizaine_ = Mathf.FloorToInt(time / 10);
+                int unite_ = Mathf.FloorToInt(time) - dizaine_ * 10;
+                dizaine.GetComponent<TMPro.TMP_Text>().text = dizaine_.ToString();
+                unite.GetComponent<TMPro.TMP_Text>().text = unite_.ToString();
+            }
+
+            else
+            {
+                dizaine.GetComponent<TMPro.TMP_Text>().text = "0";
+                unite.GetComponent<TMPro.TMP_Text>().text = time.ToString();
+            }
+
+                yield return new WaitForSeconds(1.0f);
             time -= 1;
         }
         reset.ResetPlayer(origineTime);
