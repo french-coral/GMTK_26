7478a0f3af5b43da4240e10b5a6b9c28bb8f062d
TDF360 <thibautwismer@gmail.com>
2026-07-25 01:39:05 +0200

    player + menue start

---
+++ Assets/Scripts/UI/MainMenue.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenue : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+
+
+}
