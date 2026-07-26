b733a7687e547ad6540f3b79f32a698d3afcc3fb
Mwa <166878262+french-coral@users.noreply.github.com>
2026-07-26 07:23:59 +0200

    Objective styling and rotation

--- Assets/Scripts/World/Objectif.cs
+++ Assets/Scripts/World/Objectif.cs
@@ -6,6 +6,8 @@ public class Objectif : MonoBehaviour
     public GameObject timer;
     public GameObject victoryScreen;
 
+    [SerializeField] private float spinSpeed;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
@@ -13,6 +15,11 @@ public class Objectif : MonoBehaviour
             timer.SetActive(false);
             victoryScreen.SetActive(true);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(45.0f * Time.fixedDeltaTime * spinSpeed, 45.0f * Time.fixedDeltaTime * spinSpeed, 0.0f, Space.Self);
     }
 
 }
